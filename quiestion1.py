9dc3321a05f262d937a93e857814159ddfe65c83
swetharp6 <swetharp6@gmail.com>
2024-06-07 10:16:19 -0400

    first question is completed

---
+++ quiestion1.py
@@ -0,0 +1,14 @@
+def calculator(num1,num2):
+    sum = num1 + num2
+    print(f"the sum of {num1} and {num2} is {sum}")
+    difference = num1 - num2
+    print(f"the difference between {num1} and {num2} is {difference}")
+    product = num1 * num2
+    print(f"the product of {num1} and {num2} is {product}")
+    division= num1 // num2
+    print(f"the output for floor division of {num1} to {num2} is {division}")
+    remainder = num1 % num2
+    print(f"the remainder when dividing {num1} with {num2} is {remainder}")
+    
+    
+calculator(7,2)
