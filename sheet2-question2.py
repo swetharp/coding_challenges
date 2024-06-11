5a2616fc488ae4b6694ef0d16165357a457a7d11
swetharp6 <swetharp6@gmail.com>
2024-06-11 13:25:30 -0400

    the changes suggested for question2.py was taken care of

--- sheet2-question2.py
+++ sheet2-question2.py
@@ -5,6 +5,6 @@ if num1 > num2 and num1 > num3:
     print(f"{num1} is greater than {num2} and {num3}")
 elif num2 > num1 and num2 > num3:
     print(f"{num2} is greater than {num1} and {num3}")
-elif num3 > num1 and num3 > num2 :
+else:
     print(f"{num3} is greater than {num1} and {num2}")
 
